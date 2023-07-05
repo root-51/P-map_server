eb08ee059100b434c13e5ece08bb9d892c0f2c69
JangSoyun <soyoon5328@gmail.com>
2023-07-06 00:41:26 +0900

    feat: 테스트- 서울시 실시간 인구데이터 API 연결

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.static('public'));
 const ejs = require('ejs');
 app.set('view engine', 'ejs')
 
+const xml2js = require('xml2js');
 
 //connect MongoDB
 var database;
@@ -33,6 +34,56 @@ MongoClient.connect(
     });
 
 const path = require('path');
+const openApiKey = process.env.openAPI_KEY;
+const http = require('http');
+
 app.get('/', function (request, response) {
-    response.render(path.join(__dirname, 'views', 'map.ejs'));
+    response.render(path.join(__dirname, 'views', 'map.ejs'), { areaName: '' });
+});
+
+app.get('/API', function (req, res) {
+    res.render(path.join(__dirname, 'views', 'map.ejs'), { areaName: '' });
+});
+
+
+app.get('/API/2', function (req, res) {
+    console.log('start')
+    const area_cd = "POI072";
+    const url = 'http://openapi.seoul.go.kr:8088/' + openApiKey + '/xml/citydata/1/1/' + area_cd;
+    http.get(url, (response) => {
+        let data = '';
+
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        response.on('end', () => {
+            console.log('api응답 받음')
+
+            //xml parsing
+            xml2js.parseString(data, (err, result) => {
+                if (err) {
+                    console.error('XML 파싱 오류', err);
+                    res.status(500).send('XML 파싱 오류');
+                    return;
+                }
+                const areaName = result['SeoulRtd.citydata']['CITYDATA'][0]['AREA_NM'][0];
+                const areaCongestLevel = result['SeoulRtd.citydata']['CITYDATA'][0]['LIVE_PPLTN_STTS'][0]['LIVE_PPLTN_STTS'][0]['AREA_CONGEST_LVL'][0];
+                const areaCongestMessage = result['SeoulRtd.citydata']['CITYDATA'][0]['LIVE_PPLTN_STTS'][0]['LIVE_PPLTN_STTS'][0]['AREA_CONGEST_MSG'][0];
+                const time = result['SeoulRtd.citydata']['CITYDATA'][0]['LIVE_PPLTN_STTS'][0]['LIVE_PPLTN_STTS'][0]['PPLTN_TIME'][0];
+                console.log('지역명:', areaName);
+                console.log('혼잡도:', areaCongestLevel);
+                console.log('설명:', areaCongestMessage);
+                console.log('기준시간:', time);
+                res.render(path.join(__dirname, 'views', 'map.ejs'), {
+                    areaName: areaName,
+                    areaCongestLevel: areaCongestLevel,
+                    areaCongestMessage: areaCongestMessage,
+                    time: time
+                });
+            })
+        });
+    }).on('error', (error) => {
+        console.log('API 요청 중 오류 발생:', error);
+    })
 })
